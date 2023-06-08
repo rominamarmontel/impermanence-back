f4d075717fa5a9ba295d6a173a0ccc6b329fc304
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-06-08 09:26:40 +0200

    modified

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-const path = require('path')
+// const path = require('path')
 
 const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:3000/'
 
@@ -13,16 +13,16 @@ module.exports = (app) => {
       origin: [FRONTEND_URL],
     })
   )
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200)
-    } else {
-      next()
-    }
-  })
+  // app.use((req, res, next) => {
+  //   res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL)
+  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  //   if (req.method === 'OPTIONS') {
+  //     res.sendStatus(200)
+  //   } else {
+  //     next()
+  //   }
+  // })
   app.use(logger('dev'))
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
