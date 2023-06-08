ec026c55caa1c7d8d0485547d6da3c5c68bec14d
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-06-08 10:09:08 +0200

    header modified

--- config/index.js
+++ config/index.js
@@ -13,16 +13,16 @@ module.exports = (app) => {
       origin: [FRONTEND_URL],
     })
   )
-  // app.use((req, res, next) => {
-  //   res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL)
-  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-  //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  //   if (req.method === 'OPTIONS') {
-  //     res.sendStatus(200)
-  //   } else {
-  //     next()
-  //   }
-  // })
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200)
+    } else {
+      next()
+    }
+  })
   app.use(logger('dev'))
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
