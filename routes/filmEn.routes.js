ca54de02097b88e03079f1b38607b787706a9377
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-07-02 16:05:30 +0200

    modified filmEn.routes

--- routes/filmEn.routes.js
+++ routes/filmEn.routes.js
@@ -1,8 +1,5 @@
 const router = require('express').Router()
 const FilmEn = require('../models/FilmEn.model')
-const FilmFr = require('../models/FilmFr.model')
-const fileUpload = require('../config/cloudinary-config')
-const isAuthenticated = require('../middleware/isAuthenticated')
 
 router.get('/', async (req, res, next) => {
   try {
